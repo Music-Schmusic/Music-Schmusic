5329ff716f0c4555f6854ad828694d112a27101e
joseespinoza15432 <jose.espinoza15432@gmail.com>
2025-02-11 09:37:01 -0800

    moved backend into express-backend

--- packages/express-backend/backend 2.js
+++ packages/express-backend/backend 2.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import authRoutes from './endpoints/authorize.js';
 import connectDB from "./db.js"; // Import the database connection function
 
 const app = express();
