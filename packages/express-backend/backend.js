55626ae76d1a68d9e4f10041e6315aeef50262ee
greinhard2003 <skyscreamer13@icloud.com>
2025-02-11 20:18:48 -0800

    Account creation progress

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,18 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './endpoints/authorize.js';
-import connectDB from './db.js'; // Import the database connection function
+
 
 const app = express();
 const port = process.env.PORT || 8000;
-
-// Connect to MongoDB
-connectDB();
 
 app.use(cors());
 app.use(express.json());
 app.use('/', authRoutes);
 
 app.get('/', (req, res) => res.send('API Running'));
+
+app.post('/signup', (req, res) => {
+  try {
+    const { username, email, password, spotifyid, spotifysecret } = req.body;
+  }
+})
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ packages/express-backend/db-requests.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import userModel from "./user.js";
+import connectDB from './db.js'; // Import the database connection function
+
+mongoose.set("debug", true);
+
+// Connect to MongoDB
+connectDB();
+
+function getUser(username) {
+  let promise = userModel.find({ username: username });
+  return promise;
+}
+
+exports.export getUser;
