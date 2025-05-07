35fc9a6976e0b8f28b267cf718c1cf988e5daa24
Jared Hammett <jthammet@calpoly.edu>
2025-05-06 17:46:27 -0700

    fixing config

--- packages/express-backend/jest.config.js
+++ packages/express-backend/jest.config.js
@@ -1,6 +1,5 @@
 export default {
   testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.js'],
   transform: {}, // Disable Babel by default since we're running native ESM
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1', // Needed for imports to resolve in ESM
