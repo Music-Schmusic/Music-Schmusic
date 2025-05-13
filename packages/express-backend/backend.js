de16ab5139e2b2c490f55f89e16c05ea1dae0ae8
Jhammett03 <116196579+Jhammett03@users.noreply.github.com>
2025-05-13 12:58:15 -0700

    updated app.options

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -58,8 +58,16 @@ app.use((req, res, next) => {
   next();
 });
 app.options('*', (req, res) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.sendStatus(200);
 });
+
 
 
 app.use(express.json());
