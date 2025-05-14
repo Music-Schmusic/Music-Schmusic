fc25cc25772721b91e05aceb0320eab485630c84
Jhammett03 <116196579+Jhammett03@users.noreply.github.com>
2025-05-14 15:38:51 -0700

    test

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -30,7 +30,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 dbrequests.setDataBaseConn(db());
 
